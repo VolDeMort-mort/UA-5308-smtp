9c38b5bafa1d602707dea8195fc7b6183d4501d2
SerMeSer <sr.medvediev@gmail.com>
2026-02-26 13:52:05 +0200

    added entity classes

---
+++ database/Entity/Recipient.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+struct Recipient {
+    int id;
+    int message_id;
+    std::string address;
+};
---
+++ database/Entity/Users.cpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+struct Users
+{
+    int id = 0;
+    std::string username = "";
+    std::string password_hash = "";
+};
