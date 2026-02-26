4e31d1c109dd5a315bfae982a400b6f40791ac55
SerMeSer <sr.medvediev@gmail.com>
2026-02-26 16:42:01 +0200

    entity fix

--- database/Entity/Folder.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct Folder {
-    int id;
-    int user_id;
-    std::string name;
-};
--- database/Entity/Recipient.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct Recipient {
-    int id;
-    int message_id;
-    std::string address;
-};
