96b467afc617b651b6f36196b506c5e4afbf75b5
SerMeSer <sr.medvediev@gmail.com>
2026-03-06 15:30:44 +0200

    fixed encryption functions

--- proto/src/SMTPServerSocket.cpp
+++ proto/src/SMTPServerSocket.cpp
@@ -262,37 +262,46 @@ void SMTPServerSocket::ReadDataBlock(std::chrono::milliseconds timeout, ReadHand
 void SMTPServerSocket::StartTls(boost::asio::ssl::context& ctx,
 								std::function<void(const boost::system::error_code&)> handler)
 {
-	if (m_is_tls)
-	{
-		handler(make_error_code(boost::system::errc::already_connected));
-		return;
-	}
-
-	if (m_buf.size() > 0)
-	{
-		m_buf.consume(m_buf.size());
-	}
-
-	tcp::socket raw_socket = m_socket->release_tcp_socket();
-
-	m_socket.reset();
-
-	auto ssl = std::make_unique<boost::asio::ssl::stream<tcp::socket>>(std::move(raw_socket), ctx);
-
 	auto self = shared_from_this();
-	ssl->async_handshake(
-		boost::asio::ssl::stream_base::server,
-		boost::asio::bind_executor(
-			m_strand,
-			[self, ssl = std::move(ssl), handler = std::move(handler)](const boost::system::error_code& ec) mutable
+
+	boost::asio::dispatch(
+		m_strand,
+		[self, &ctx, handler = std::move(handler)]() mutable
+		{
+			if (self->m_is_tls)
 			{
-				if (ec)
-				{
-					handler(ec);
-					return;
-				}
-				self->m_socket = std::make_unique<SslStream>(std::move(*ssl));
-				self->m_is_tls = true;
-				handler(ec);
-			}));
+				handler(make_error_code(boost::system::errc::already_connected));
+				return;
+			}
+
+			if (self->m_buf.size() > 0)
+			{
+				self->m_buf.consume(self->m_buf.size());
+			}
+
+			tcp::socket raw_socket = self->m_socket->release_tcp_socket();
+			self->m_socket.reset();
+
+			auto ssl = std::make_unique<boost::asio::ssl::stream<tcp::socket>>(std::move(raw_socket), ctx);
+
+			ssl->async_handshake(
+				boost::asio::ssl::stream_base::server,
+				boost::asio::bind_executor(
+					self->m_strand,
+					[self, ssl = std::move(ssl),
+					 handler = std::move(handler)](const boost::system::error_code& ec) mutable
+					{
+						if (ec)
+						{
+							handler(ec);
+							return;
+						}
+
+						self->m_socket = std::make_unique<SslStream>(std::move(*ssl));
+
+						self->m_is_tls = true;
+
+						handler(ec);
+					}));
+		});
 }
