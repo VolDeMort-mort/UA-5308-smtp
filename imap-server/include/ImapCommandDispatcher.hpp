b6ad6fe441f688e88da1e84d9b428c0c20dde452
Yevhen <kutelmahz@gmail.com>
2026-03-07 14:04:31 +0000

    Refactore code regarding handling commands via dispatch pattern for better clarity and testability

---
+++ imap-server/include/ImapCommandDispatcher.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <optional>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "ImapCommand.hpp"
+#include "ImapSessionTypes.hpp"
+#include "Logger.h"
+#include "Repository/MessageRepository.h"
+#include "Repository/UserRepository.h"
+
+class ImapCommandDispatcher
+{
+public:
+	ImapCommandDispatcher(ILogger& logger, UserRepository& userRepo, MessageRepository& messRepo);
+
+	std::string Dispatch(const ImapCommand& cmd);
+	bool RequiresAuth(ImapCommandType type) const;
+
+	SessionState get_State() const { return m_state; }
+	void set_State(SessionState state) { m_state = state; }
+
+	std::optional<int64_t> get_AuthenticatedUserID() const { return m_authenticatedUserID; }
+	const std::string& get_AuthenticatedUserName() const { return m_authenticatedUserName; }
+	const MailboxState& get_MailboxState() const { return m_currentMailbox; }
+
+private:
+	ILogger& m_logger;
+	UserRepository& m_userRepo;
+	MessageRepository& m_messRepo;
+
+	SessionState m_state = SessionState::NonAuthenticated;
+	std::string m_authenticatedUserName;
+	std::optional<int64_t> m_authenticatedUserID;
+	MailboxState m_currentMailbox;
+
+	std::string HandleLogin(const ImapCommand& cmd);
+	std::string HandleLogout(const ImapCommand& cmd);
+	std::string HandleCapability(const ImapCommand& cmd);
+	std::string HandleNoop(const ImapCommand& cmd);
+	std::string HandleSelect(const ImapCommand& cmd);
+	std::string HandleList(const ImapCommand& cmd);
+	std::string HandleLsub(const ImapCommand& cmd);
+	std::string HandleStatus(const ImapCommand& cmd);
+	std::string HandleFetch(const ImapCommand& cmd);
+	std::string HandleStore(const ImapCommand& cmd);
+	std::string HandleCreate(const ImapCommand& cmd);
+	std::string HandleDelete(const ImapCommand& cmd);
+	std::string HandleRename(const ImapCommand& cmd);
+	std::string HandleCopy(const ImapCommand& cmd);
+	std::string HandleExpunge(const ImapCommand& cmd);
+
+	std::map<ImapCommandType, std::function<std::string(const ImapCommand&)>> m_handlers;
+};
--- imap-server/include/ImapSession.hpp
+++ imap-server/include/ImapSession.hpp
@@ -11,32 +11,16 @@
 
 #include "DataBaseManager.h"
 #include "ImapCommand.hpp"
+#include "ImapCommandDispatcher.hpp"
+#include "ImapSessionTypes.hpp"
 #include "Logger.h"
 #include "Repository/MessageRepository.h"
 #include "Repository/UserRepository.h"
 
-struct MailboxState
-{
-	std::string m_name;
-	size_t m_exists = 0;
-	size_t m_recent = 0;
-	size_t m_unseen = 0;
-	std::set<std::string> m_flags;
-	std::optional<int64_t> m_id; // folder id in db, needed for operations like fetch, store, etc
-	bool m_readOnly = false;
-};
-
-enum class SessionState
-{
-	NonAuthenticated,
-	Authenticated,
-	Selected
-};
-
 class ImapSession : public std::enable_shared_from_this<ImapSession>
 {
 public:
-	ImapSession(boost::asio::ip::tcp::socket socket, Logger& logger, DataBaseManager& db, UserDAL& u_dal);
+	ImapSession(boost::asio::ip::tcp::socket socket, ILogger& logger, DataBaseManager& db, UserDAL& u_dal);
 	void Start();
 
 private:
@@ -44,34 +28,13 @@ private:
 	void ReadCommand();
 	void HandleCommand(const std::string& line);
 	void WriteResponse(const std::string& msg);
-	bool RequiresAuth(ImapCommandType type) const;
-
-	std::string HandleLogin(const ImapCommand& cmd);
-	std::string HandleLogout(const ImapCommand& cmd);
-	std::string HandleCapability(const ImapCommand& cmd);
-	std::string HandleNoop(const ImapCommand& cmd);
-	std::string HandleSelect(const ImapCommand& cmd);
-	std::string HandleList(const ImapCommand& cmd);
-	std::string HandleLsub(const ImapCommand& cmd);
-	std::string HandleStatus(const ImapCommand& cmd);
-	std::string HandleFetch(const ImapCommand& cmd);
-	std::string HandleStore(const ImapCommand& cmd);
-	std::string HandleCreate(const ImapCommand& cmd);
-	std::string HandleDelete(const ImapCommand& cmd);
-	std::string HandleRename(const ImapCommand& cmd);
-	std::string HandleCopy(const ImapCommand& cmd);
-	std::string HandleExpunge(const ImapCommand& cmd);
-
-	std::map<ImapCommandType, std::function<std::string(const ImapCommand&)>> m_handlers;
 
 	boost::asio::ip::tcp::socket m_socket;
 	boost::asio::streambuf m_buffer;
-	Logger& m_logger;
+	ILogger& m_logger;
+
 	MessageRepository m_mess_repo;
 	UserRepository m_user_repo;
 
-	SessionState m_state = SessionState::NonAuthenticated;
-	std::string m_authenticatedUserName;
-	std::optional<int64_t> m_authenticatedUserID;
-	MailboxState m_currentMailbox;
+	std::unique_ptr<ImapCommandDispatcher> m_dispatcher;
 };
